975128159f3f838611fd43ebae0a71bb53954795
syphax.bouazzouni <gs_bouazzouni@esi.dz>
2021-01-13 07:47:55 +0100

    deploy problems 5

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -10,5 +10,5 @@
 <link rel="stylesheet" href="/dist/styles.64719975a986d17169a6.css"></head>
 <body>
   <app-root></app-root>
-<script src="/dist/runtime.1e880f27c6796bf4bed5.js" defer></script><script src="/dist/polyfills.2e71067f259e79e852da.js" defer></script><script src="/dist/scripts.1c89aa03a98913aba97b.js" defer></script><script src="/dist/main.b48fb3cbfdfa8d787e54.js" defer></script></body>
+<script src="/dist/runtime.1e880f27c6796bf4bed5.js" defer></script><script src="/dist/polyfills.2e71067f259e79e852da.js" defer></script><script src="/dist/scripts.1c89aa03a98913aba97b.js" defer></script><script src="/dist/main.3ddfa6bb36098386f610.js" defer></script></body>
 </html>
