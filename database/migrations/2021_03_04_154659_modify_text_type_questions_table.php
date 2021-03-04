b5c5bdc29bdf46aa947b5e1748bf3a760537c26e
syphax.bouazzouni <gs_bouazzouni@esi.dz>
2021-03-04 15:54:27 +0100

    change question text size to text

---
+++ database/migrations/2021_03_04_154659_modify_text_type_questions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyTextTypeQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->text('text')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
