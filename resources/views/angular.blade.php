08385f555dc1bfabeb4f56d1d074d870fd0b65fc
syphax.bouazzouni <gs_bouazzouni@esi.dz>
2021-03-20 10:09:51 +0100

    test login 2

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -12,5 +12,5 @@
 <link rel="stylesheet" href="/dist/styles.ff054ec9e1edc495fbf5.css"></head>
 <body class="mat-typography">
   <app-root></app-root>
-<script src="/dist/runtime.1e880f27c6796bf4bed5.js" defer=""></script><script src="/dist/polyfills.3116b8e9dbb90fea460e.js" defer=""></script><script src="/dist/scripts.08746a71a825faa1fa03.js" defer=""></script><script src="/dist/main.9ea2f300bc9c055d2a57.js" defer=""></script></body>
+<script src="/dist/runtime.1e880f27c6796bf4bed5.js" defer=""></script><script src="/dist/polyfills.3116b8e9dbb90fea460e.js" defer=""></script><script src="/dist/scripts.08746a71a825faa1fa03.js" defer=""></script><script src="/dist/main.87a20fb96ddfefa4f2f7.js" defer=""></script></body>
 </html>
