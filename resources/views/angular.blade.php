45115cce3d496374086407ace3473d6c4413fbc5
syphax.bouazzouni <gs_bouazzouni@esi.dz>
2021-04-07 17:41:18 +0100

    mobile design for no admin fix

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -12,5 +12,5 @@
 <link rel="stylesheet" href="/dist/styles.3511542d83332b5c5186.css"></head>
 <body class="mat-typography">
   <app-root></app-root>
-<script src="/dist/runtime.1e880f27c6796bf4bed5.js" defer=""></script><script src="/dist/polyfills.3116b8e9dbb90fea460e.js" defer=""></script><script src="/dist/scripts.08746a71a825faa1fa03.js" defer=""></script><script src="/dist/main.626cf53f6bcd9a6d4616.js" defer=""></script></body>
+<script src="/dist/runtime.1e880f27c6796bf4bed5.js" defer=""></script><script src="/dist/polyfills.3116b8e9dbb90fea460e.js" defer=""></script><script src="/dist/scripts.08746a71a825faa1fa03.js" defer=""></script><script src="/dist/main.6d417660dc672df73c11.js" defer=""></script></body>
 </html>
